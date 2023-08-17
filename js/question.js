ac2e260f25bad04f967c1c952013fedb19aa7f09
mimiprosper <mimiprosper@hotmail.com>
2023-08-17 13:57:19 +0100

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "what does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "what does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Cascading Sheet Scripts",
+      "Cascading Sheet Script",
+      "Coding Sheet Scripts",
+      "Cascading Sheet Server",
+    ],
+  },
+
+  {
+    numb: 3,
+    question: "what does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+      "Hypertext Preprocessor",
+    ],
+  },
+
+  {
+    numb: 4,
+    question: "what does SQL stand for?",
+    answer: "Structure Query Language",
+    options: [
+      "Style Query Language",
+      "Structure Question Language",
+      "Structure Query Language",
+      "Statement Query Language",
+    ],
+  },
+
+  {
+    numb: 5,
+    question: "what does XML stand for?",
+    answer: "Extensible Markup Language",
+    options: [
+      "Executable Markup Language",
+      "Extensible Markup Language",
+      "Extensible Mark Language",
+      "Expensive Markup Language",
+    ],
+  },
+];
